0ade466efda7d73d4ec99b4e653e3e819135a1fe
apon55 <90351560+apon55@users.noreply.github.com>
2021-09-13 03:06:47 +0600

    Add files via upload

---
+++ Assignment 1-3 class/function.js
@@ -0,0 +1,6 @@
+function changetext (){
+document.getElementById('originaltext').innerHTML = 'Corona is a virus';
+document.getElementById('originaltext').style.color='red';
+document.getElementById('originaltext').style.backgroundColor='rgb(121, 250, 164';
+document.getElementById('originaltext').style.fontSize='45px';
+}
